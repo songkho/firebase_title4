26301c8c8a387f2df0fcfbe83ea85e8fb6e07409
songkho <songkho12@naver.com>
2020-01-18 00:06:40 +0900

    sss

---
+++ app/src/main/java/com/idsoft/firebase_title/signedin.java
@@ -0,0 +1,14 @@
+package com.idsoft.firebase_title;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signedin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signedin);
+    }
+}
