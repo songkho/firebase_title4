8935d6864f16dcef6b78ff69802b1ec0129dd5c8
songkho <songkho12@naver.com>
2020-01-30 19:12:41 +0900

    파이어스토어2

--- app/src/main/java/com/idsoft/firebase_title/cloudstorage/ColudStorageActivity.java
+++ app/src/main/java/com/idsoft/firebase_title/cloudstorage/ColudStorageActivity.java
@@ -1,16 +1,103 @@
 package com.idsoft.firebase_title.cloudstorage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.idsoft.firebase_title.R;
 
-public class ColudStorageActivity extends AppCompatActivity {
+public class ColudStorageActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private final int REQUEST_CODE = 100;
+
+    private Button uploadbtn;
+    private Button downloadbtn;
+    private Button metainfobtn;
+    private Button deletebtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colud_storage);
+
+
+        uploadbtn = findViewById(R.id.uploadbtn);
+        uploadbtn.setOnClickListener(this);
+
+        downloadbtn = findViewById(R.id.downloadbtn);
+        downloadbtn.setOnClickListener(this);
+
+        metainfobtn = findViewById(R.id.metainfobtn);
+        metainfobtn.setOnClickListener(this);
+
+        deletebtn = findViewById(R.id.deletebtn);
+        deletebtn.setOnClickListener(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE);
+                Toast.makeText(this, "안드로이드 6.0부터 마시멜로부터 일부권한에 대해 사용자에게 동의 필요!", Toast.LENGTH_LONG).show();
+
+                uploadbtn.setEnabled(false);
+                downloadbtn.setEnabled(false);
+                metainfobtn.setEnabled(false);
+                deletebtn.setEnabled(false);  // 버튼 비활성화
+            }
+        }
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i = null;
+
+        switch (view.getId()) {
+            case R.id.uploadbtn:
+                i = new Intent(this, UploadActivity.class);
+                break;
+            case R.id.downloadbtn:
+                i = new Intent(this, UploadActivity.class);
+                break;
+            default:
+                break;
+        }
+
+        if (i !=null){
+            startActivity(i);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                    uploadbtn.setEnabled(true);
+                    downloadbtn.setEnabled(true);
+                    metainfobtn.setEnabled(true);
+                    deletebtn.setEnabled(true);
+
+
+                }
+                break;
+                default:
+                    break;
+        }
     }
 }
--- app/src/main/java/com/idsoft/firebase_title/cloudstorage/UploadActivity.java
+++ app/src/main/java/com/idsoft/firebase_title/cloudstorage/UploadActivity.java
@@ -1,16 +1,135 @@
 package com.idsoft.firebase_title.cloudstorage;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.idsoft.firebase_title.R;
 
-public class UploadActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int REQ_CODE_SELECT_IMAGE = 1000;
+
+    private String mImgPath = null;
+    private String mImgTitle = null;
+    private String mImgOrient = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload);
+
+        Button uploadbtn = findViewById(R.id.imguploadbtn);
+
+        uploadbtn.setOnClickListener(this);
+
+        getGallery();
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == REQ_CODE_SELECT_IMAGE){
+            if (resultCode == Activity.RESULT_OK){
+                Uri uri = data.getData();
+                getImageNameToUri(uri);
+
+                try {
+                    Bitmap bm = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    ImageView img = findViewById(R.id.showimg);
+                    img.setImageBitmap(bm);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.imguploadbtn:
+                uploadFile(mImgPath);
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
+
+
+    private void getGallery(){
+        Intent intent = null;
+
+        if (Build.VERSION.SDK_INT >= 19){
+            intent =  new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        }else {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+
+        }
+
+        intent.setType("image/*");
+        startActivityForResult(intent, REQ_CODE_SELECT_IMAGE);
+    }
+
+    //uri 정보를 이용하여 사진정보를 가져오기 위한것
+
+
+
+    private void getImageNameToUri(Uri data){
+        String[] proj = {
+                MediaStore.Images.Media.DATA,
+                MediaStore.Images.Media.TITLE,
+                MediaStore.Images.Media.ORIENTATION
+        };
+
+        Cursor cursor = this.getContentResolver().query(data,  proj, null, null, null);
+        cursor.moveToFirst();// Cursor를 첫번째 행으로 이동
+
+                // Cursor는 데이터 베이스에 저장되어 있는 데이터들을 가져와서 테이블을 이루고 있는 행(Row)을 참조하여 사용을 할 수 있게 해줍니다.
+        //DB는 테이블로 구성 행과열로 구성 Cursor는 행을 참조
+
+        int column_data = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        int column_title = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.TITLE);
+        int column_orientation = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.ORIENTATION);
+
+        mImgPath = cursor.getString(column_data);
+        mImgTitle = cursor.getString(column_title);
+        mImgOrient = cursor.getString(column_orientation);
+
+        //
+        // getColumnIndexOrThrow()
+        // 특정 필드의 인덱스값을 반환하며, 필드가 존재하지 않을경우 예외를 발생시킵니다.
+
+        Log.d("namjinha", "mImgPath = " + mImgPath);
+        Log.d("namjinha", "mImgTitle = " + mImgTitle);
+        Log.d("namjinha", "mImgOrient = " + mImgOrient);
+
+    }
+
+    private void uploadFile(String aFilePath) {
+
+
+
+    }
+
+
+
+
 }
