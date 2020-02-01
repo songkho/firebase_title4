c6a10ae41c85750b01b17ae0f7d21cb6464fc42e
song <songkho12@naver.com>
2020-02-01 12:37:20 +0900

    2020.02.01 집컴퓨터에서 커밋 푸시2

---
+++ app/src/main/java/com/idsoft/firebase_title/cloudstorage/MetaInfoActivity.java
@@ -0,0 +1,17 @@
+package com.idsoft.firebase_title.cloudstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MetaInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meta_info);
+
+
+
+    }
+}
